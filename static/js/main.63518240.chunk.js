6faae082fe2f823bbd97ea149d37b7735643691d
Chaos <rizwannafees13@gmail.com>
2021-08-03 16:05:30 -0400

    Updates

---
+++ static/js/main.63518240.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpshopping-cart"]=this["webpackJsonpshopping-cart"]||[]).push([[0],{2:function(p,s,t){"use strict";t.r(s);t(0),t(4)}},[[2,1,2]]]);
+//# sourceMappingURL=main.63518240.chunk.js.map
