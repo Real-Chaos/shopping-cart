bed740654dae51177db7ab64163d39a6d660e758
chaos <rizwannafees13@gmail.com>
2021-08-10 19:11:22 -0400

    removed width state

--- src/Components/UI/Product Details/ShirtDetails.js
+++ src/Components/UI/Product Details/ShirtDetails.js
@@ -11,19 +11,10 @@ const ShirtDetails = ({ match }) => {
     const checkShirt = shirts.filter(shirt => shirt.name.toLowerCase().split(" ").join("-") === match.params.id ? shirt.img: null)
     let [productImg, setProductImg] = useState(checkShirt[0].allImages[0].src);
 
-    const [ imgWidth, setImgWidth] = useState(400)
   
     let allImages = [...checkShirt[0].allImages]
     const handleClick = (e) => {
         setProductImg(e.target.src)
-        switch(imgWidth) {
-            case 400:
-                setImgWidth(401)
-                break;
-            case 401:
-                setImgWidth(400)
-                break;
-        }
     }
 
 
@@ -41,7 +32,7 @@ const ShirtDetails = ({ match }) => {
                 <Zoom
                   img={productImg}
                   zoomScale={3}
-                  width={imgWidth}
+                  width={400}
                   height={400}
                   transitionTime={0.5}
                   className="product-img"
